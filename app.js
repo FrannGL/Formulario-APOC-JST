9ba2770d9f1aa2bb1860c3c091ba9cee339d3121
Franco Galluccio <francoivangalluccio@gmail.com>
2023-02-27 14:42:02 -0300

    Version 1.2

---
+++ app.js
@@ -0,0 +1,43 @@
+const btnSend = document.getElementById('button-send');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btnSend.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_z2xr16g';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+        Toastify({
+            text: "Formulario enviado Correctamente",
+            duration: 2000,
+            gravity: "bottom",
+            position: "right",
+            style: {
+                background: "linear-gradient(to right, #00484b, #0088b6)",
+            },
+        }).showToast();
+    }, (err) => {
+      btnSend.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+const btnReset = document.getElementById('button-reset');
+btnReset.addEventListener('click', () => {
+    Toastify({
+        text: "Formulario Reiniciado",
+        duration: 2000,
+        gravity: "bottom",
+        position: "right",
+        style: {
+            background: "linear-gradient(to right, #88000b, #9200b6)",
+        },
+    }).showToast();
+});
+
+
+
